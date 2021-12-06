efdcab16343c91b6ccf609c024294f5957e7cf94
FaustTobias <tf@ambimax.de>
2021-12-06 10:30:11 +0100

    fix: Exposed variables are now printed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import core from "@actions/core";
 import { escapeBranchName } from "./escapeBranchName";
 
+console.log("Environment variables:");
+console.log("GITHUB_HEAD_REF", process.env.GITHUB_HEAD_REF);
+console.log("GITHUB_REF", process.env.GITHUB_REF);
+
 const ref = process.env.GITHUB_HEAD_REF ?? process.env.GITHUB_REF;
 const branchName = ref?.slice(11);
 const escapedBranchName = escapeBranchName(branchName);
+
+console.log("Outputs:");
+console.log("branch_name: ", branchName);
+console.log("escaped_branch_name: ", escapedBranchName);
 
 core.setOutput("ref", ref);
 core.setOutput("branchName", branchName);
